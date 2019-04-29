39658931d8e0ca8f6e24a0afb39ea7b5b5447e0b
zq <2466877980@qq.com>
2019-04-30 07:57:15 +0800

    commit

---
+++ js/购物车.js
@@ -0,0 +1,34 @@
+var app =new Vue({
+    el:'#app',
+    data:{
+       list: [{
+            id:1,
+            name:'iphone 7',
+            price:6188,
+            count:1
+            },
+            {
+                id:2,
+                name:'ipad pro',
+                price:5888,
+                count:1
+            },
+           {
+               id:3,
+               name:'MacBook Pro',
+               price:21488,
+               count:1
+
+           }
+
+        ]},
+    computed:{
+
+    },
+    methods:{
+
+    }
+
+
+
+})
