dcbde359123e474b87fa0275ae81faba12d5a0c7
zq <2466877980@qq.com>
2019-05-02 00:29:31 +0800

    vue基础提交

--- js/购物车.js
+++ js/购物车.js
@@ -1,34 +1,53 @@
-var app =new Vue({
-    el:'#app',
-    data:{
-       list: [{
-            id:1,
-            name:'iphone 7',
-            price:6188,
-            count:1
+var app = new Vue({
+    el: '#app',
+    data: {
+        list: [{
+            id: 1,
+            name: 'iphone 7',
+            price: 6188,
+            count: 1
+        },
+            {
+                id: 2,
+                name: 'ipad pro',
+                price: 5888,
+                count: 1
             },
             {
-                id:2,
-                name:'ipad pro',
-                price:5888,
-                count:1
-            },
-           {
-               id:3,
-               name:'MacBook Pro',
-               price:21488,
-               count:1
+                id: 3,
+                name: 'MacBook Pro',
+                price: 21488,
+                count: 1
 
-           }
+            }
 
-        ]},
-    computed:{
+        ]
+    },
+    computed: {
+        totalPrice: function () {
+            let total = 0;
+            for (let i = 0; i < this.list.length; i++) {
+                total += this.list[i].count * this.list[i].price;
+
+            }
+            return total.toString().replace(/\B(?=(\d{3})+$)/g, ',');
+            // return total
+
+        }
 
     },
-    methods:{
+    methods: {
 
+        handleReduce:function (index) {
+            this.list[index].count--
+        },
+        handleIncrease:function (index) {
+            this.list[index].count++;
+        },
+        handleRemove:function (index) {
+            this.list.splice(index, 1);  //会改变原数组
+        }
     }
-
 
 
 })
